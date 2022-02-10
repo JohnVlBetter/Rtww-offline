3388cf1ca1886218d7cfea647e0c137c397534e2
John <lijiang01001@163.com>
2022-02-10 22:14:16 +0800

    triangle bug fix

--- src/rtww/main/main.cpp
+++ src/rtww/main/main.cpp
@@ -321,8 +321,6 @@ ShapesSet Geometry() {
 	objects.Add(CreateSphere(Point3f(195, 120, 250), Vector3f(40, 40, 40), Vector3f(), aluminum));
 	objects.Add(CreateSphere(Point3f(305, 120, 250), Vector3f(40, 40, 40), Vector3f(), glass));
 	objects.Add(std::make_shared<Triangle>(Vector3f(195, 120, -100), Vector3f(500, 120, -100), Vector3f(380, 500, 0), green));
-	objects.Add(std::make_shared<RectangleXZ>(std::make_shared<Transform>(
-		Point3f(380, 120, 250), Vector3f(156, 1, 131), Vector3f()),green));
 	
 	return objects;
 }
@@ -336,7 +334,7 @@ ShapesSet Triangles() {
 	objects.Add(std::make_shared<FlipFace>(std::make_shared<RectangleXZ>(std::make_shared<Transform>(
 		Point3f(0, 554, -100), Vector3f(156, 1, 131), Vector3f()), light)));
 	std::shared_ptr<Material> aluminum = std::make_shared<Metal>(Color(0.5, 0.5, 0.6), 0.0);
-	objects.Add(std::make_shared<Triangle>(Vector3f(-50, -50, 0), Vector3f(-50, 60, 0), Vector3f(100, 0, 0), red));
+	objects.Add(std::make_shared<Triangle>(Vector3f(-50, -50, 0), Vector3f(-50, 60, 100), Vector3f(100, 0, 0), red));
 
 	return objects;
 }
@@ -400,36 +398,36 @@ int main(int argc, char** argv) {
 	//
 	//renderer.Render(Draw, 0, 1);
 
-	//auto lights = std::make_shared<ShapesSet>();
-	//lights->Add(std::make_shared<RectangleXZ>(std::make_shared<Transform>(
-	//	Point3f(400, 700, 0), Vector3f(300, 1, 300), Vector3f(0, 0, 0)), std::shared_ptr<Material>()));
-	//fs::path imageParentPath("D:/Workspace/CG/Repos/Rtww-offline/build/x64/Release/Geometry");
-	//if (!fs::exists(imageParentPath)) {
-	//	fs::create_directory(imageParentPath);
-	//}
-	//FrameRenderer renderer("Geometry", imageParentPath, 20, 8);
-	//
-	//Color background(0, 0, 0);
-	//Vector3f vup(0, 1, 0);
-	//Point3f lookfrom = Point3f(270, 250, -550);
-	//Point3f lookat = Point3f(200, 200, 400);
-	//auto vfov = 40.0;
-	//auto dist2Focus = 10.0f;
-	//auto aperture = 0.0;
-	//
-	//auto settings = std::make_shared<FrameSettings>();
-	//settings->SetImageOptions(400, 400);
-	//settings->SetRayTraceOptions(50, 100);
-	//settings->SetScene(std::make_shared<Camera>(lookfrom, lookat, vup, vfov, 1.0f, aperture, dist2Focus),
-	//	std::make_shared<ShapesSet>(Geometry()), lights, background);
-	//renderer.AddFrame(settings);
-	//
-	//renderer.Render(Draw, 0, 1);
+	auto lights = std::make_shared<ShapesSet>();
+	lights->Add(std::make_shared<RectangleXZ>(std::make_shared<Transform>(
+		Point3f(400, 700, 0), Vector3f(300, 1, 300), Vector3f(0, 0, 0)), std::shared_ptr<Material>()));
+	fs::path imageParentPath("D:/Workspace/CG/Repos/Rtww-offline/build/x64/Release/Geometry");
+	if (!fs::exists(imageParentPath)) {
+		fs::create_directory(imageParentPath);
+	}
+	FrameRenderer renderer("Geometry", imageParentPath, 20, 8);
+	
+	Color background(0, 0, 0);
+	Vector3f vup(0, 1, 0);
+	Point3f lookfrom = Point3f(270, 250, -550);
+	Point3f lookat = Point3f(200, 200, 400);
+	auto vfov = 40.0;
+	auto dist2Focus = 10.0f;
+	auto aperture = 0.0;
+	
+	auto settings = std::make_shared<FrameSettings>();
+	settings->SetImageOptions(400, 400);
+	settings->SetRayTraceOptions(50, 100);
+	settings->SetScene(std::make_shared<Camera>(lookfrom, lookat, vup, vfov, 1.0f, aperture, dist2Focus),
+		std::make_shared<ShapesSet>(Geometry()), lights, background);
+	renderer.AddFrame(settings);
+	
+	renderer.Render(Draw, 0, 1);
 	//Model model;
 	//model.Load("D:/Workspace/CG/Repos/Rtww-offline/resources/models/cornell_box.obj");
 	//system("pause");
 
-	auto lights = std::make_shared<ShapesSet>();
+	/*auto lights = std::make_shared<ShapesSet>();
 	lights->Add(std::make_shared<RectangleXZ>(std::make_shared<Transform>(
 		Point3f(0, 554, -100), Vector3f(156, 1, 131), Vector3f(0, 0, 0)), std::shared_ptr<Material>()));
 	fs::path imageParentPath("D:/Workspace/CG/Repos/Rtww-offline/build/x64/Release/Triangle");
@@ -453,6 +451,6 @@ int main(int argc, char** argv) {
 		std::make_shared<ShapesSet>(Triangles()), lights, background);
 	renderer.AddFrame(settings);
 	
-	renderer.Render(Draw, 0, 1);
+	renderer.Render(Draw, 0, 1);*/
 	return 0;
 }
--- src/rtww/shape/Triangle.hpp
+++ src/rtww/shape/Triangle.hpp
@@ -14,38 +14,35 @@ bool Triangle::BoundingBox(Float time0, Float time1, AABB& outputBox) const {
 	return false;
 }
 
-bool IntersectTriangle(const Vector3f& orig, const Vector3f& dir, const Vector3f& v0, const Vector3f& v1, const Vector3f& v2, float* t, float* u, float* v) {
-	Vector3 E1 = v1 - v0;
-	Vector3 E2 = v2 - v0;
-	Vector3 P = Cross(dir, E2);
-	float det = Dot(E1, P);
-	Vector3f T;
-	if (det > 0) T = orig - v0;
-	else {
-		T = v0 - orig;
-		det = -det;
+bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f& v2, const Vector3f& orig,
+	const Vector3f& dir, float& tnear, float& u, float& v)
+{
+	bool isIn = false;
+	Vector3f E1 = v1 - v0;
+	Vector3f E2 = v2 - v0;
+	Vector3f S = orig - v0;
+	Vector3f S1 = Cross(dir, E2);
+	Vector3f S2 = Cross(S, E1);
+	float coeff = 1.0 / Dot(S1, E1); // 共同系数
+	float t = coeff * Dot(S2, E2);
+	float b1 = coeff * Dot(S1, S);
+	float b2 = coeff * Dot(S2, dir);
+	if (t >= 0 && b1 >= 0 && b2 >= 0 && (1 - b1 - b2) >= 0)
+	{
+		isIn = true;
+		tnear = t;
+		u = b1;
+		v = b2;
 	}
 
-	if (det < 0.0001f) return false;
-	*u = Dot(T, P);
-	if (*u < 0.0f || *u > det) return false;
-
-	Vector3f Q = Cross(T, E1);
-	*v = Dot(dir, Q);
-	if (*v < 0.0f || *u + *v > det) return false;
-	*t = Dot(E2, Q);
-	float fInvDet = 1.0f / det;
-	*t *= fInvDet;
-	*u *= fInvDet;
-	*v *= fInvDet;
-
-	return true;
+	return isIn;
 }
+
 
 bool Triangle::Intersection(const Ray & r, Float tMin, Float tMax, IntersectionRecord & rec) const {
 	Vector3f o(r.origin.x, r.origin.y, r.origin.z);
 	float t, u, v;
-	bool result = IntersectTriangle(o, r.direction, p1, p2, p3, &t, &u, &v);
+	bool result = rayTriangleIntersect(p1, p2, p3, o, r.direction, t, u, v);
 	rec.time = t;
 	rec.hitPoint = r.At(t);
 	Vector3 E1 = p2 - p1;
